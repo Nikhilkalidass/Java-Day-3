a989556c822deb0804bb8a0efdf707dc19a991ec
Nikhilkalidass <nikhilkalidass0709@gmail.com>
2025-03-19 15:24:59 +0530

    Add files via upload

---
+++ Stringsexample.java
@@ -0,0 +1,13 @@
+package Session3;
+
+public class Stringsexample {
+public static void main(String[] args) {
+	String s1="mugilan:";//string literals
+	String s2=new String("Moses sir");//String object
+	char[] c1= {'J','A','V','A'};
+	String s3=new String(c1);
+	System.out.println(s1);
+	System.out.println(s2);
+	System.out.println(s3);
+}
+}
